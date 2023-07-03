3afd9e440dad3e183befc1a2a108d4619d7b1d7b
19Alvaro <120038490+19Alvaro@users.noreply.github.com>
2023-07-03 14:47:06 -0500

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,12 @@
+class operaciones:
+    def suma(x,y):
+        return x+y
+    def resta(x,y):
+        return x- y
+    def producto(x,y):
+        return x * y
+    def division(x,y):
+        return x/y
+print(operaciones.suma(10,10))
+      
+    
